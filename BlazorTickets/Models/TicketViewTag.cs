11b28b4fdb02b81a4f59230ee555eb1ff404202e
Adirsflo <rida.abdal@gmail.com>
2024-02-22 11:20:22 +0100

    Viewmodels added

---
+++ BlazorTickets/Models/TicketViewTag.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorTickets.Models
+{
+    public class TicketViewTag
+    {
+        public int TicketId { get; set; }
+        public TicketViewModel Ticket { get; set; }
+        public int TagId { get; set; }
+        public TagViewModel Tag { get; set; } // Sparas som ett heltal (int) i databasen(enumens value),
+                                              // går sedan att omvandla till det motsvarande namnet
+    }
+}
