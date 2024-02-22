1c073636e68d6daba2ea45c52b21f80ba10898e3
JosefMontanari <josef.montanari94@gmail.com>
2024-02-22 10:57:53 +0100

    Add controllers

---
+++ BlazorTickets.DAL/Controllers/ResponseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorTickets.DAL.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ResponseController : ControllerBase
+	{
+		//private DbContext context; /*Fältvariabel för dependency injection databas*/
+		public List<ResponseModel>? Responses { get; set; }
+
+		[HttpGet]
+		public ActionResult<List<ResponseModel>> Get()
+		{
+			//Responses = context.Responses.ToList(); // Hämta alla responses i databasen och lägg dessa i listan
+			return Ok(Responses);
+		}
+		// Posta en ny respons
+		[HttpPost]
+		public ActionResult PostResponse(ResponseModel responseModel)
+		{
+			if (responseModel != null)
+			{
+				//context.Responses.Add(responseModel); /*Lägg till responsen*/
+				//context.SaveChanges();
+				return Ok();
+
+			}
+			return BadRequest();
+		}
+
+	}
+}
