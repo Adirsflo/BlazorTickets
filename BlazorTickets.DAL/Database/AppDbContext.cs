b90b29dd035b875edfdc6f97e49400c62bd20a84
Henk01 <146067073+Henk01@users.noreply.github.com>
2024-02-22 13:02:44 +0100

    DbContext/relations

---
+++ BlazorTickets.DAL/Database/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.ViewModels;
+
+namespace BlazorTickets.DAL.Database
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext()
+		{
+
+		}
+
+		public DbSet<TicketModel> Tickets { get; set; }
+		public DbSet<ResponseModel> Responses { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BlazorTickets;Trusted_Connection=True;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<TicketTagModel>()
+			.HasKey(tt => new { tt.TicketId, tt.TagId });
+
+			modelBuilder.Entity<TicketTagModel>()
+			.HasOne(tt => tt.Ticket)
+			.WithMany(t => t.TicketTags)
+			.HasForeignKey(tt => tt.TicketId);
+
+			modelBuilder.Entity<TicketTagModel>()
+			.HasOne(tt => tt.Tag)
+			.WithMany(t => t.TicketTags)
+			.HasForeignKey(tt => tt.TagId);
+
+			modelBuilder.Entity<ResponseModel>()
+			.HasOne(r => r.Ticket)
+			.WithMany(t => t.Responses)
+			.HasForeignKey(r => r.TicketId);
+		}
+
+	}
+}
--- Shared/ViewModels/TagViewModel.cs
+++ Shared/ViewModels/TagViewModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace Shared.ViewModels
 {
-	internal class TagModel
+	public class TagModel
 	{
 		public int Id { get; set; }
-		public string Name { get; set; } // Exempel: "CSharp", "JavaScript"
+		public string Name { get; set; }
 		public List<TicketTagModel> TicketTags { get; set; } = new List<TicketTagModel>();
 
 	}
